a9a7cf897ac47ab8a22051d18894a4c647f6210a
Prabakar <86585391+prxbxkax@users.noreply.github.com>
2023-01-06 17:53:32 +0000

    Update skin_cancer.py

--- models/skin_cancer.py
+++ models/skin_cancer.py
@@ -1,234 +1,103 @@
-#!/usr/bin/env python
-
-# coding: utf-8
-
-# In[1]:
-
-import pathlib
-
-import tensorflow as tf
-
-import matplotlib.pyplot as plt
-
-import numpy as np
-
-import pandas as pd
-
+import gc
 import os
-
-from PIL import Image
-
-from tensorflow import keras
-
-from keras import layers
-
-from keras.models import Sequential
-
-np.random.seed(42)
-
-from keras.utils.np_utils import to_categorical
-
-import sklearn
-
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Flatten,Dense,Dropout,BatchNormalization,Conv2D,MaxPooling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.metrics import Recall
+from tensorflow.keras.optimizers import RMSprop
+import matplotlib.pyplot as p
+import PIL as pil
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import to_categorical,normalize
 from sklearn.model_selection import train_test_split
+from keras import regularizers
+import seaborn as sns
+from tensorflow.keras.layers import Flatten,Dense,Dropout,BatchNormalization,Conv2D,MaxPooling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.metrics import Recall
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.callbacks import Callback,ReduceLROnPlateau
+from sklearn.metrics import classification_report,confusion_matrix
 
-from scipy import stats
-
-from sklearn.preprocessing import LabelEncoder
-
-from sklearn.utils import resample
-
-from glob import glob
-
-print("imported")
-
-print(tf.__version__)
-
-print(keras.__version__)
-
-# In[3]:
-
-scdf=pd.read_csv("E:\dataset\HAM10000_metadata.csv")
-
-# In[4]:
-
-l=LabelEncoder()
-
-l.fit(scdf['dx'])
-
-LabelEncoder()
-
-print(list(l.classes_))
-
-# In[5]:
-
-scdf['label']=l.transform(scdf['dx'])
-
-# In[6]:
-
-print(scdf['label'].value_counts())
-
-# In[7]:
-
-df0=scdf[scdf['label']==0]
-
-df1=scdf[scdf['label']==1]
-
-df2=scdf[scdf['label']==2]
-
-df3=scdf[scdf['label']==3]
-
-df4=scdf[scdf['label']==4]
-
-df5=scdf[scdf['label']==5]
-
-df6=scdf[scdf['label']==6]
-
-# In[8]:
-
-n=5500
-
-df0b=resample(df0,replace=True,n_samples=n,random_state=42)
-
-df1b=resample(df1,replace=True,n_samples=n,random_state=42)
-
-df2b=resample(df2,replace=True,n_samples=n,random_state=42)
-
-df3b=resample(df3,replace=True,n_samples=n,random_state=42)
-
-df4b=resample(df4,replace=True,n_samples=n,random_state=42)
-
-df5b=resample(df5,replace=True,n_samples=n,random_state=42)
-
-df6b=resample(df6,replace=True,n_samples=n,random_state=42)
-
-# In[9]:
-
-scdfb=pd.concat([df0b,df1b,df2b,df3b,df4b,df5b,df6b])
-
-# In[10]:
-
-print(scdfb['label'].value_counts())
-
-# In[11]:
-
-imgpath={os.path.splitext(os.path.basename(x))[0]: x
-
-        for x in glob(os.path.join('E:\\dataset\\','*','*.jpg'))}
-
-# In[12]:
-
-scdfb['path']=scdf['image_id'].map(imgpath.get)
-
-# In[13]:
-
-scdfb['image']=scdfb['path'].map(lambda x: np.asarray(Image.open(x).resize((32,32))))
-
-# In[14]:
-
-x=np.asarray(scdfb['image'].tolist())
-
+base="skin_cancer\\HAM10000_images_part_1"
+metadata=pd.read_csv(os.path.join(base,'HAM10000_metadata.csv'))
+df=pd.read_csv(os.path.join(base,'hmnist_28_28_RGB.csv'))
+x=df.drop('label',axis=1)
+y=df['label']
+x=x.to_numpy()
 x=x/255
+y=to_categorical(y)
+label={
+    ' Actinic keratoses':0,
+    'Basal cell carcinoma':1,
+    'Benign keratosis-like lesions':2,
+    'Dermatofibroma':3,
+    'Melanocytic nevi':4,
+    'Melanoma':6,
+    'Vascular lesions':5
+}
+x=x.reshape(-1,28,28,3)
+trainx,trainy,testx,testy = train_test_split(x,y,test_size=0.05,random_state=42)
+data_generator=ImageDataGenerator(rotation_range=20,
+                               width_shift_range=0.10,
+                               height_shift_range=0.10,
+                               rescale=1/255,
+                               shear_range=0.1,
+                               zoom_range=0.1,
+                               horizontal_flip=True,
+                               vertical_flip=True,
+                               fill_mode='nearest')
+data_generator.fit(trainx)
+from tensorflow.keras.metrics import categorical_accuracy, top_k_categorical_accuracy
+def accuracy(y_true, y_pred):
+    return top_k_categorical_accuracy(y_true, y_pred, k=2)
 
-y=scdfb['label']
-
-yc=to_categorical(y,num_classes=7)
-
-trainx,testx,trainy,testy=train_test_split(x,yc,test_size=0.25,random_state=42)
-
-nc=7
-
-# In[15]:
-
-model= tf.keras.Sequential()
-
-model.add(tf.keras.layers.Conv2D(256,(3,3),activation="relu",input_shape=(32,32,3)))
-
-model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2)))
-
-model.add(tf.keras.layers.Dropout(0.3))
-
-model.add(tf.keras.layers.Conv2D(128,(3,3),activation="relu"))
-
-model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2)))
-
-model.add(tf.keras.layers.Dropout(0.3))
-
-model.add(tf.keras.layers.Conv2D(64,(3,3),activation="relu"))
-
-model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2)))
-
-model.add(tf.keras.layers.Dropout(0.3))
-
-model.add(tf.keras.layers.Flatten())
-
-model.add(tf.keras.layers.Dense(32))
-
-model.add(tf.keras.layers.Dense(7,activation="softmax"))
-
+model=Sequential()
+model.add(Conv2D(64,(2,2),input_shape=(28,28,3),activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+model.add(Conv2D(512,(2,2),input_shape=(28,28,3),activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+model.add(Dropout(0.3))
+model.add(Conv2D(1024,(2,2),input_shape=(28,28,3),activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+model.add(Dropout(0.3))
+model.add(Conv2D(1024,(1,1),input_shape=(28,28,3),activation='relu'))
+model.add(MaxPooling2D(pool_size=(1, 1)))
+model.add(BatchNormalization())
+model.add(Dropout(0.3))
+model.add(Conv2D(1024,(1,1),input_shape=(28,28,3),activation='relu'))
+model.add(MaxPooling2D(pool_size=(1, 1)))
+model.add(BatchNormalization())
+model.add(Dropout(0.3))
+model.add(Conv2D(2048,(1,1),input_shape=(28,28,3),activation='relu'))
+model.add(MaxPooling2D(pool_size=(1,1)))
+model.add(BatchNormalization())
+model.add(Dropout(0.3))
+model.add(Conv2D(2048,(1,1),input_shape=(28,28,3),activation='relu'))
+model.add(MaxPooling2D(pool_size=(1,1)))
+model.add(BatchNormalization())
+model.add(Dropout(0.3))
+model.add(Conv2D(4096,(1,1),input_shape=(28,28,3),activation='relu'))
+model.add(MaxPooling2D(pool_size=(1,1)))
+model.add(BatchNormalization())
+model.add(Dropout(0.3))
+model.add(Flatten())
+model.add(Dense(256,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7,activation='softmax'))
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=[accuracy])
 model.summary()
 
-# In[16]:
+class GCCollect(Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        gc.collect()
 
-model.compile(loss='categorical_crossentropy',optimizer='Adam',metrics=['acc'])
-
-# In[17]:
-
-history=model.fit(trainx,trainy,epochs=70,batch_size=100,shuffle=True,validation_data=(testx,testy),verbose=2)
-
-# In[18]:
-
-loss=history.history['loss']
-
-val_loss=history.history['val_loss']
-
-epochs=range(1,len(loss)+1)
-
-plt.plot(epochs,loss,'y',label='Training loss')
-
-plt.plot(epochs,val_loss,'r',label='Validation loss')
-
-plt.title("Losses")
-
-plt.xlabel('Epochs')
-
-plt.ylabel('Loss')
-
-plt.legend()
-
-plt.show()
-
-# In[19]:
-
-acc=history.history['acc']
-
-val_acc=history.history['val_acc']
-
-plt.plot(epochs,acc,'y',label="Training accuracy")
-
-plt.plot(epochs,val_acc,'r',label='Validation accuracy')
-
-plt.title('Accuracy')
-
-plt.xlabel('Epochs')
-
-plt.ylabel('Accuracy')
-
-plt.legend()
-
-plt.show()
-
-# In[25]:
-
-prediction=model.predict(testx)
-
-print(prediction[1])
-
-# In[26]:
-
-# In[29]:
-
-model.save('skin_cancer_detector.h5')
-
-# In[ ]:
-
+reduce_lr = ReduceLROnPlateau(monitor='accuracy', factor=0.5, patience=2, verbose=1,cooldown=0, mode='auto',min_delta=0.0001, min_lr=0)
+class_weights = {0:1,1:0.5,2:1,3:1,4:1,5:1,6:1}
+model.fit(trainx,testx,epochs=35,batch_size=90,class_weight=class_weights,validation_data=(trainy, testy),callbacks=[GCCollect(), reduce_lr])
+model.save("skin.h5")
